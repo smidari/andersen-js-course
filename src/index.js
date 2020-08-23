67519e74c9485f588dd761378cea7fdb13cb0735
Dasha <smidariaa@gmail.com>
2020-08-23 17:05:24 +0300

    Done the task7 from array-practice

--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ import forEachRight from './array-practice/task3';
 import union from './array-practice/task4';
 import createGenerator from './array-practice/task5';
 import transformArrayToNumber from './array-practice/task6';
+import take from './array-practice/task7';
 
 // array-practice
 
@@ -55,6 +56,10 @@ console.log(transformArrayToNumber([10, 20, 30], (acc, item) => acc + item, 10))
 console.log(transformArrayToNumber([10, 20, 30], (acc, item) => acc * item)); // -> 0
 console.log(transformArrayToNumber([10, 20, 30], (acc, item) => acc * item, 1)); // -> 6000
 console.log(transformArrayToNumber([10, 20, 30], (acc, item) => acc - item)); // -> -60
+
+// task 7
+console.log(take([1, 2, 3], 5)); // -> [1, 2, 3]
+console.log(take([1, 2, 3], 2)); // -> [1, 2]
 // import { task1Old, task1New } from './es-features/task1';
 // import { task2Old, task2New } from './es-features/task2';
 // import { task3Old, task3New } from './es-features/task3';
